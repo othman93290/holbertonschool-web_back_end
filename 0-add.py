0fb8e28b2c9345bb1eb2ab03738d1b0d98422c48
othman93290 <7566@holbertonstudent.com>
2024-09-12 15:18:02 +0200

    add

---
+++ 0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" a type-annotated function add that takes a float a and a float b """
+
+
+def add(a: float, b: float) -> float:
+    """ returns their sum as a float """
+    return a + b
