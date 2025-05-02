bd9665a8589aae59c5317a49974f63580204834f
othman93290 <7566@holbertonstudent.com>
2025-05-02 14:55:32 +0200

    12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,7 +4,11 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb+srv://alfredgibeauahoussinou:<Cbm1Umz23y4HUS7o>@cluster0.ntikegm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    client = MongoClient(
+        'mongodb+srv://alfredgibeauahoussinou:'
+        '<Cbm1Umz23y4HUS7o>@cluster0.ntikegm.mongodb.net/'
+        '?retryWrites=true&w=majority&appName=Cluster0'
+    )
     db_nginx = client.logs.nginx
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
@@ -19,5 +23,4 @@ if __name__ == "__main__":
     check = db_nginx.count_documents(
         {"method": "GET", "path": "/status"}
     )
-
     print(f'{check} status check')
