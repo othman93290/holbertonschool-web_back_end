5e3cb284b4006869d47cbbe9f11eff647d3912cf
othman193290 <7566@holbertonstudents.com>
2025-05-28 21:40:28 +0200

    tt

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,19 +1,11 @@
-// Importer le module Express
 const express = require('express');
 
-// Créer une application Express
 const app = express();
 
-// Définir une route pour l'endpoint /
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!'); // Envoyer la réponse
+  res.send('Hello Holberton School!');
 });
 
-// Faire écouter l'application sur le port 1245
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(1245);
 
-// Exporter l'application
 module.exports = app;
