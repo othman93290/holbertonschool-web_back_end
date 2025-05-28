b6d05cd8128c6e1540439b6797b898a53248d303
othman193290 <7566@holbertonstudents.com>
2025-05-28 20:54:31 +0200

    tt serv

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,44 @@
+const { readDatabase } = require('../utils');
+
+class StudentsController {
+  static getAllStudents(req, res) {
+    const databaseFile = process.argv[2];
+    readDatabase(databaseFile)
+      .then((students) => {
+        let responseText = 'This is the list of our students\n';
+        Object.keys(students)
+          .sort()
+          .forEach((field) => {
+            responseText += `Number of students in ${field}: ${
+              students[field].length
+            }. List: ${students[field].join(', ')}\n`;
+          });
+        res.status(200).send(responseText.trim());
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(req, res) {
+    const databaseFile = process.argv[2];
+    const { major } = req.params;
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+    readDatabase(databaseFile)
+      .then((students) => {
+        if (!students[major]) {
+          res.status(500).send('Cannot load the database');
+          return;
+        }
+        res.status(200).send(`List: ${students[major].join(', ')}`);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+}
+
+module.exports = StudentsController;
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises;
+
+async function readDatabase(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    const lines = data
+      .trim()
+      .split('\n')
+      .slice(1); // Ignorer la première ligne
+    const result = {};
+
+    for (const line of lines) {
+      const [firstName, , , field] = line.split(','); // Extraire les valeurs des colonnes
+      if (!result[field]) {
+        result[field] = [];
+      }
+      result[field].push(firstName);
+    }
+
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+module.exports = { readDatabase };
